4259e2b7dd5ccf48cfbcc091e618c682709769eb
Dinesh Santosh Khairnar <96805747+dskhairnar@users.noreply.github.com>
2025-04-07 11:33:44 +0530

    Delete src directory

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Sample route
-router.get('/', (req, res) => {
-  res.json({ message: 'User route is working!' });
-});
-
-module.exports = router;
